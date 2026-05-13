12856bfdf282ea4a7fef088174bcb96a7548c222
edwardqs <ventas1@pauserdistribucionessac.onmicrosoft.com>
2026-05-13 11:49:29 -0500

    cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173"],
+  origin: [
+    "http://localhost:5173", 
+    "http://localhost:5174", 
+    "http://127.0.0.1:5173",
+    "https://pauserdistribucionessac.com",
+    process.env.FRONTEND_URL || ""
+  ].filter(Boolean),
   credentials: true,
 }));
 
